493752ba7d0c208ef92023a3f003dc2f99a05d63
Cheemma1 <chidimma9521@gmail.com>
2025-08-07 06:35:51 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-montserat",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Fintrack App",
+  description: "A Dashboard app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
