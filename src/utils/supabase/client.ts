8f6bd56f446f32597cd49e4fe149c7617b105f26
Cheemma1 <chidimma9521@gmail.com>
2025-09-05 17:03:53 +0100

    fix:bug-fix

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,15 @@
-import { createBrowserClient } from "@supabase/ssr";
+// import { createBrowserClient } from "@supabase/ssr";
 
-export const supabase = createBrowserClient(
+// export const supabase = createBrowserClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// );
+
+// utils/supabase/client.ts ✅
+
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
